9e0efbb027851341971e21b24e372b45cb675a8e
mobuffon <moritzvonbuchwaldt@web.de>
2025-10-29 19:51:20 +0100

    adding arrow direction based on wind direction

--- src/components/MapMarker.jsx
+++ src/components/MapMarker.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 import Map, {Marker} from 'react-map-gl/mapbox';
 
+function getArrowRotation(direction)  {
+    switch(direction) {
+        case 'N': return 90;
+        case 'NE': return 135;
+        case 'E': return 180;
+        case 'SE': return 225;
+        case 'S': return 270;
+        case 'SW': return 315;
+        case 'W': return 0;
+        case 'NW': return 45;
+        default: return 0;
+    }
+}
+
+
 function MapMarker({ spot_name, wind_direction, wind_power, latitude, longitude }) {
+  
   const borderColor = wind_power >25 ? 'red' : wind_power > 15 ? 'green' : 'blue';
   return (    
     <Marker latitude={latitude} longitude={longitude}>
@@ -14,7 +30,13 @@ function MapMarker({ spot_name, wind_direction, wind_power, latitude, longitude 
         boxShadow: '0 0 5px rgba(0,0,0,0.3)'
       }}>
         <strong>{spot_name}</strong>
-        <div>{wind_direction} {wind_power} knts</div>
+        <br />
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <div style={{ transform: `rotate(${getArrowRotation(wind_direction)}deg)`, padding: '0 8px' }}>
+                ➤
+            </div>
+            <span> {wind_power} knts</span>
+        </div>
       </div>
     </Marker>
   );
