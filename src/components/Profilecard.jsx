20a2bb63c9a5b056ce8373f0696546ece813e403
sille97 <sillefeltsen@hotmail.com>
2025-11-17 21:50:15 +0100

    added service file for reading the current profile

--- src/components/Profilecard.jsx
+++ src/components/Profilecard.jsx
@@ -1,23 +1,24 @@
 import React from "react";
-import ava1 from "../assets/avatar1.png";
+import defaultAvatar from "../assets/Default.png";
 import "../styles/Profilecard.css";
 
 export default function ProfileCard({
   firstName = "",
   lastName = "",
-  //name = "",
   age,
   skillLevel,
-  avatar = ava1,
+  avatar = defaultAvatar,
 }) {
-  const displayName = firstName || name || "";
+  const displayName = firstName || "User";
+  const safeAvatar = avatar || defaultAvatar;
 
   return (
     <div className="profile-card">
       <div className="profile-image-container">
         <img
-          src={avatar}
+          src={safeAvatar}
           alt={displayName ? `${displayName} avatar` : "Profile"}
+          onError={(e) => (e.target.src = defaultAvatar)} // If the profile doesn't have a picture
         />
       </div>
       <h2 className="profile-name">{displayName}</h2>
