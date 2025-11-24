2f1c358438c1207d40f5396efa9556d106559a4c
greta67-create <greta.wette@gmail.com>
2025-11-24 16:23:01 +0100

    smaller changes

--- src/pages/Profileview.jsx
+++ src/pages/Profileview.jsx
@@ -118,7 +118,7 @@ export default function ProfileView({ onLogout }) {
                 <Link
                   key={s.id}
                   to={`/session/${s.id}`}
-                  style={{ textDecoration: "none" }}
+                  className="session-link"
                 >
                   <Sessionblock
                     key={s.id}
